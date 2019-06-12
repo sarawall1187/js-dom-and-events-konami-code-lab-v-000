41dcb65e0c01449926368feb8e43eec4b5b97015
sarawall1187 <sarawall1187@gmail.com>
2019-06-12 10:39:52 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+ 
+function init() {
+  // const input = document.querySelector('body');
+  const key = parseInt(e.detail || e.which);
+  console.log(e.which)
+  console.log('1st break')
+  console.log(e.location)
+  console.log('2nd break')
+  console.log(e.detail)
+  console.log('end')
+  // input.addEventListener('keydown', function(e) {
+
+  //  });
 }
+
+// init()
+
