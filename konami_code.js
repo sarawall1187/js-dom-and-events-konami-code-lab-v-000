f194597a9d1ad2cd4bffd996896cfa680f385192
Sara Walker <sarawall1187@gmail.com>
2019-06-12 18:17:06 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
  
 function init() {
+<<<<<<< HEAD
   // const input = document.querySelector('body');
   const key = parseInt(e.detail || e.which);
   console.log(e.which)
@@ -18,3 +19,21 @@ function init() {
 
 // init()
 
+=======
+  let index = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+     if (key === code[index]) {
+       index++;
+      if (index === code.length) {
+       alert("Congratulations!");
+       index = 0;
+      }
+    }
+    else {
+    index = 0;
+  };
+ })
+}
+init()
+>>>>>>> 628bc807cdf0e475ef45fbf2e1d1164341880a4c
